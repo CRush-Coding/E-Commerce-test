8b65b78cae681a6e9f62385475f63e2c7ffc6a69
CRush-Coding <blazeboy2213@gmail.com>
2025-01-08 18:35:01 +1100

    initial commit

---
+++ controller/userCtrl.js
@@ -0,0 +1,18 @@
+const User = require('../models/userModel');
+const asyncHandler = require('express-async-handler');
+
+const createUser = asyncHandler(async (req, res) => {
+    const email = req.body.email;
+    const findUser = await User.findOne({email: email});
+
+    if (!findUser) {
+        // Create a new user
+        const newUser = await User.create(req.body);
+        res.json(newUser);
+    } else {
+        // User already created
+        throw new Error('User Already Exists');
+    }
+});
+
+module.exports = {createUser};
