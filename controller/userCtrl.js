70d5c2a9c7d2c602ce5c588d6edf7a9b7e6b5e11
CRush-Coding <blazeboy2213@gmail.com>
2025-01-09 14:26:04 +1100

    Added multiple user functionalitites, including create, delete, view, jsonwebtokens and password hashing

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,3 +1,4 @@
+const { generateToken } = require('../config/jwToken');
 const User = require('../models/userModel');
 const asyncHandler = require('express-async-handler');
 
@@ -20,10 +21,61 @@ const loginUserCtrl = asyncHandler( async(req, res) => {
     // Check if user exists or not
     const findUser = await User.findOne({email});
     if (findUser && await findUser.isPasswordMatched(password)) {
-        res.json(findUser)
+        res.json({
+            _id: findUser?._id,
+            firstname: findUser?.firstname,
+            lastname: findUser?.lastname,
+            email: findUser?.email,
+            mobile: findUser?.mobile,
+            token: generateToken(findUser?.id),
+        })
     } else {
         throw new Error("Invalid Credentials");
     }
 });
 
-module.exports = {createUser, loginUserCtrl};
+
+// Get all users
+
+const getallUser = asyncHandler( async(req, res) => {
+   try {
+    const getUsers = await User.find();
+    res.json(getUsers);
+   }
+   catch (error) {
+    throw new Error(error);
+   }
+});
+
+
+// Get a single user
+
+const getsingleUser = asyncHandler( async(req, res) => {
+    const {id} = req.params;
+    try {
+        const getsingleUser = await User.findById(id);
+        res.json({
+            getsingleUser,
+        });
+    }
+    catch (error) {
+        throw new Error(error);
+    }
+});
+
+const deleteUser = asyncHandler( async(req, res) => {
+    const {id} = req.params;
+    try {
+        const deleteUser = await User.findByIdAndDelete(id);
+        res.json({
+            deleteUser,
+        });
+    }
+    catch (error) {
+        throw new Error(error);
+    }
+});
+
+
+
+module.exports = {createUser, loginUserCtrl, getallUser, getsingleUser, deleteUser};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router();
-const { createUser, loginUserCtrl } = require("../controller/userCtrl");
+const { createUser, loginUserCtrl, getallUser, getsingleUser, deleteUser } = require("../controller/userCtrl");
 
 router.post("/register", createUser);
 router.post("/login", loginUserCtrl);
+router.get("/all-users", getallUser);
+router.get("/:id", getsingleUser);
+router.delete("/:id", deleteUser);
+
 
 module.exports = router;
