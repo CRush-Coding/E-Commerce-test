611705e148b2f8951f4645dd8571ff135fb3a630
CRush-Coding <blazeboy2213@gmail.com>
2025-01-17 15:48:38 +1100

    beginning blogs

--- controller/blogCtrl.js
+++ controller/blogCtrl.js
@@ -1,5 +1,11 @@
 const blog = require('../models/blogModels');
 const User = require('../models/userModel');
 const asyncHandler = require("express-async-handler");
+const validateMongoDbId = require("../utils/validateMongodbd")
 
+const createBlog = asyncHandler(async(req,res) => {
+    
 
+});
+
+module.exports = {createBlog};
